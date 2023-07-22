7c1be8bec2b2a873523ef0f96e4d8de2f1948d1f
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-07-22 12:50:01 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import smtplib
+
+my_email = "From Email Address"
+password = "Email Password"
+connection = smtplib.SMTP("smtp.gmail.com") #Depends upon you mail id
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="to Email Address", msg="Subject:Hello\n\nEesaraina Poora")
+connection.close()
